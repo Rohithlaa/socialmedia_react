a869858581cd9bb85c376d9ce656a4753183bc0b
indianic <indianic@indianics-iMac-Pro.local>
2021-10-08 15:07:22 +0530

    Side bar component completed

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,146 @@
+import React from 'react'
+import './sidebar.css'
+
+import { Bookmark, Chat, Event, Group, QuestionAnswer, RssFeed, School, VideoCallRounded, Work } from '@material-ui/icons'
+
+export default function Sidebar() {
+    return (
+        <div className="Sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Chars</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <VideoCallRounded className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Group className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Bookmark className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Bookmark</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <QuestionAnswer className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Work className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Job</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Event className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Event</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <School className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Bookmark</span>
+                    </li>
+
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr"/>
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Rohith</span>
+                    </li>
+                    
+                
+                </ul>
+            </div>
+        </div>
+    )
+}
